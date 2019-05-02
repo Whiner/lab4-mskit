ea2d7c754c08a4bab17103ea8c07f9fd86913bce
whine <sashazemzer@gmail.com>
2019-05-02 18:02:59 +0300

    Initial commit

---
+++ src/main/java/org/donntu/knt/mskit/lab4/Main.java
@@ -0,0 +1,11 @@
+package org.donntu.knt.mskit.lab4;
+
+/**
+ * @author Shilenko Alexander
+ */
+public class Main {
+    public static void main(String[] args) {
+        MD4 md4 = new MD4();
+        md4.engineDigest();
+    }
+}
